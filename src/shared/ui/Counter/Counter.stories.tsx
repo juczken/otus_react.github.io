92735a3983eda29c519d81429020631b46bcf0cf
TerpigorevSA <118421759+TerpigorevSA@users.noreply.github.com>
2024-10-24 00:02:40 +0500

    refactor: Counter
    use css variable
    add:
      - onIncrement()=>void
      - onDecrement()=>void
      - onInputChange(value:number)=>void
    modify stories

---
+++ src/shared/ui/Counter/Counter.stories.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import type { Meta, StoryContext, StoryFn, StoryObj } from '@storybook/react';
+import Counter, { CounterProps } from './Counter';
+import '../../../app/App.css';
+import { count } from 'console';
+
+// const meta: Meta<typeof Counter> = {
+//   title: 'Shared/Counter',
+//   component: Counter,
+//   tags: ['autodocs'],
+//   parameters: {
+//   },
+//   decorators: [(story) => (
+//     <div style={{ height: '30px', width: '100px'}}>
+//       {story()}
+//     </div>)],
+// };
+
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+
+// export const SimpleCounter: Story = {
+//   args: {
+//     count: 0,
+//     disabled: false,
+//   }
+// }
+
+const Template = (args: CounterProps) => {
+  const [count, setCount] = useState(0);
+
+  const onIncrement = () => setCount(count + 1);
+  const onDecrement = () => setCount(count - 1);
+  const onInputChange = (value: number) => setCount(value);
+
+  return (
+    <div style={{ height: '30px', width: '100px' }}>
+      <Counter {...args} count={count} onIncrement={onIncrement} onDecrement={onDecrement} onInputChange={onInputChange} />
+    </div>
+  );
+};
+
+const meta: Meta<typeof Template> = {
+  title: 'Shared/Counter',
+  component: Template,
+  tags: ['autodocs'],
+  parameters: {
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const SimpleCounter: Story = {
+  args: {
+    count: 0,
+    disabled: false,
+  }
+}
+
+
+export const LimitedCounter = Template.bind({});
+LimitedCounter.args = {
+  count: 0,
+  min: 0,
+  max: 5,
+  disabled: false,
+};
--- src/shared/ui/Counter/Counter.tsx
+++ src/shared/ui/Counter/Counter.tsx
@@ -1,22 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import cn from 'clsx';
 import style from './Counter.module.css';
+import { on } from 'events';
 
-type CounterProps = {
+export type CounterProps = {
   count: number;
   min?: number;
   max?: number;
   disabled?: boolean;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
+  onInputChange?: (value: number) => void;
 };
 
-const Counter: FC<CounterProps> = ({ count, min, max, disabled = true }) => {
+const Counter: FC<CounterProps> = ({ count, min, max, disabled = true, onIncrement = () => { }, onDecrement = () => { }, onInputChange = (value: number) => { } }) => {
+  // const [countValue, setCountValue] = useState(count);
+  console.log({ caption: 'component', count, min, max, disabled, onIncrement, onDecrement, onInputChange })
+  console.log({ style: !disabled && (min === undefined || count > min), disabled: disabled || (min && count <= min) })
+  console.log({ style: !disabled && (max === undefined || count < max), disabled: disabled || (max && count >= max) })
   return (
     <div className={cn(style.wrapper)}>
-      <button className={cn(style.counter, { [style.counter_active]: !disabled })} disabled={disabled}>
+      <button
+        onClick={() => onDecrement()}
+        className={cn(style.counter, { [style.counter_enable]: !disabled && (min === undefined || count > min) })}
+        disabled={disabled || (min !== undefined && count <= min)}
+      >
         -
       </button>
-      <input className={cn(style.input)} type="number" value={count} disabled={disabled} />
-      <button className={cn(style.counter, { [style.counter_active]: !disabled })} disabled={disabled}>
+      <input
+        className={cn(style.input)}
+        type="number"
+        value={count}
+        disabled={disabled}
+        onChange={(event) =>
+          (max === undefined || Number(event.target.value) <= max)
+            && (min === undefined || Number(event.target.value) >= min)
+            ? onInputChange(Number(event.target.value))
+            : event.target.value = count.toString()
+        }
+      />
+      <button
+        onClick={() => onIncrement()}
+        className={cn(style.counter, { [style.counter_enable]: !disabled && (max === undefined || count < max) })}
+        disabled={disabled || (max !== undefined && count >= max)}
+      >
         +
       </button>
     </div>
