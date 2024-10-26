ca5b5ec05d23a71580c9b378d23f83a420ee6a56
TerpigorevSA <118421759+TerpigorevSA@users.noreply.github.com>
2024-10-26 22:27:37 +0500

    Add ThemeSwitcherViewer

---
+++ src/stories/ThemeSwitcherViewer/ThemeSwitcherViewer.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import ThemeSwitcher from "../../shared/ui/ThemeSwitcher/ThemeSwitcher";
+import React from "react";
+import ThemeProvider from "../../shared/providers/ThemeProvider/ThemeProvider";
+import ProductItem from "../../entities/Product/ui/ProductItem/ProductItem";
+import '../../app/App.css'
+
+
+const ThemeSwitcherViewer:FC=()=>{
+    
+    return (
+        <>
+        <ThemeProvider>
+            <div style={{height:'30px', width:'300px'}}>
+            <ThemeSwitcher/>
+            </div>
+            <hr/>
+            <ProductItem name="Lorem ipsum" price={19.95} desc="Lorem ipsum dolor sit amet consectetur adipiscing elit nostra." photo="https://via.placeholder.com/500.png?text=Lorem+ipsum"/>
+        </ThemeProvider>
+        </>
+    )
+}
+
+export default ThemeSwitcherViewer
