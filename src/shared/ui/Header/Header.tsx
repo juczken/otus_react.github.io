b4a79d63ed16f2bed5dac58603b57ee81a2007bf
juczken <juczken@mail.ru>
2024-12-27 08:47:50 +0000

    feat: Add NavBar and Route

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import style from './Header.module.css';
-import Logo from '../Logo/Logo';
-import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
-import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 
-const Header: React.FC = () => {
-  return (
-    <div className={style.header}>
-      <div className={style.item}>
-        <Logo />
-      </div>
-      <div className={style.right_wrapper}>
-        <div className={style.item}>
-          <ThemeSwitcher />
-        </div>
-        <div className={style.item}>
-          <LanguageSwitcher />
-        </div>
-      </div>
-    </div>
-  );
+type HeaderProps = {
+  children: ReactNode;
+};
+
+const Header: React.FC<HeaderProps> = ({ children }) => {
+  return <div className={style.header}>{children}</div>;
 };
 
 export default Header;
--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -2,6 +2,10 @@ import React, { FC, ReactNode } from 'react';
 import cn from 'clsx';
 import Header from '../Header/Header';
 import style from './Layout.module.css';
+import Logo from '../Logo/Logo';
+import NavigationBar from '../NavigationBar/NavigationBar';
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
+import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 
 type LayoutProps = {
   children?: ReactNode;
@@ -10,7 +14,22 @@ type LayoutProps = {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={cn(style.Layout)}>
-      <Header />
+      <Header>
+        <div className={style.item}>
+          <Logo />
+        </div>
+        <div>
+          <NavigationBar menuItems={[]} />
+        </div>
+        <div className={style.right_wrapper}>
+          <div className={style.item}>
+            <ThemeSwitcher />
+          </div>
+          <div className={style.item}>
+            <LanguageSwitcher />
+          </div>
+        </div>
+      </Header>
       {children}
     </div>
   );
