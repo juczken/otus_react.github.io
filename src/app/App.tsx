73d2d9a8ab9dd7a8a1a6e6f067b372525bd8295d
juczken <juczken@mail.ru>
2024-12-28 01:47:06 +0000

    debug

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import cn from 'clsx';
 import style from './App.css';
 import ThemeProvider from '../shared/providers/ThemeProvider/ThemeProvider';
-import Layout from '../shared/ui/Layout/Layout';
 import './localization';
 import { LanguageProvider } from '../shared/providers/LanguageProvider/LanguageProvider';
 import { menuItems } from 'src/shared/ui/Layout/menuItems';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   const generateRoutes = (items: typeof menuItems) => {
@@ -14,14 +13,17 @@ function App() {
       console.log(item);
       if (item.dropdown) {
         return (
-          <React.Fragment key={item.path+item.label}>
+          <React.Fragment key={item.path + item.label}>
             <Route path={item.path} element={item.element} />
             {generateRoutes(item.dropdown)}
           </React.Fragment>
         );
       }
-      console.log((<Route key={item.path + item.label} path={item.path} element={item.element} />).type, (<Route key={item.path + item.label} path={item.path} element={item.element} />).key)
-      return <Route key={item.path+item.label} path={item.path} element={item.element} />;
+      console.log(
+        (<Route key={item.path + item.label} path={item.path} element={item.element} />).type,
+        (<Route key={item.path + item.label} path={item.path} element={item.element} />).key
+      );
+      return <Route key={item.path + item.label} path={item.path} element={item.element} />;
     });
   };
   console.log('app');
@@ -30,11 +32,11 @@ function App() {
       <ThemeProvider>
         <LanguageProvider>
           <div className={cn(style.App)}>
-            <Routes>
+            {/* <Routes>
               <Route path="/" element={<Layout />}>
-                {/*generateRoutes(menuItems)*/}
+                {generateRoutes(menuItems)}
               </Route>
-            </Routes>
+            </Routes> */}
           </div>
         </LanguageProvider>
       </ThemeProvider>
--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -6,8 +6,6 @@ import Logo from '../Logo/Logo';
 import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 import { Outlet } from 'react-router-dom';
-import { menuItems } from './menuItems';
-import NavigationBar from '../NavigationBar/NavigationBar';
 
 const Layout: FC = () => {
   return (
@@ -16,9 +14,7 @@ const Layout: FC = () => {
         <div className={style.item}>
           <Logo />
         </div>
-        <div className={style.item}>
-          {/*<NavigationBar menuItems={menuItems} />*/}
-        </div>
+        <div className={style.item}>{/*<NavigationBar menuItems={menuItems} />*/}</div>
         <div className={style.right_wrapper}>
           <div className={style.item}>
             <ThemeSwitcher />
